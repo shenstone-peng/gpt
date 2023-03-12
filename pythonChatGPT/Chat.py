6ca7231af9b2fddd6805fe974aabf1da9fad82b0
shenstone-peng <489439220@qq.com>
2023-03-12 06:41:51 +0000

    update

--- pythonChatGPT/Chat.py
+++ pythonChatGPT/Chat.py
@@ -15,4 +15,8 @@ while True:
     
     reply = chat.choices[0].message.content
     print(f"ChatGPT: {reply}")
+    with open('data.txt', 'a') as file:
+        file.write("Q:" + message +"\n")
+        file.write("A:" + reply + "\n")
+        file.write("\n")
     messages.append({"role": "assistant", "content": reply})
