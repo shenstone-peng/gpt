41d19e07b0c29ed52d21fb1be8f6168239cbe52c
shenstone-peng <489439220@qq.com>
2023-03-13 14:10:29 +0000

    openAI refactor v1

--- pythonChatGPT/Chat.py
+++ pythonChatGPT/Chat.py
@@ -1,23 +1,28 @@
 import openai
 import os
-messages = [
-    {"role": "system", "content": "You are a kind helpful assistant."},
-]
+
+# Initialize the OpenAI API key from an environment variable
 openai.api_key = os.environ.get('OPENAIKEY')
-while True:
-    message = input("User : ")
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    
+
+# Define a function to get a response from OpenAI given a message and a list of previous messages
+def get_response(message, messages):
+    messages.append({"role": "user", "content": message})
+    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    with open('data.txt', 'a') as file:
-        file.write("Q:" + message +"\n")
-        file.write("A:" + reply + "\n")
-        file.write("\n")
     messages.append({"role": "assistant", "content": reply})
+    return reply, messages
+
+# Define the initial messages to start the conversation
+messages = [{"role": "system", "content": "You are a kind helpful assistant."}]
+
+# Start the conversation loop
+while True:
+    message = input("User: ")
+    if message:
+        reply, messages = get_response(message, messages)
+        print(f"ChatGPT: {reply}")
+        with open('data.txt', 'a') as file:
+            file.write("Q:" + message + "\n")
+            file.write("A:" + reply + "\n\n")
+    else:
+        break
